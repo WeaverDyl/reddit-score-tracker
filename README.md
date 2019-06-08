850b8c9b09420d3e8372083c73fa3eca6903e9ed
WeaverDyl <dweaver@umd.edu>
2019-06-08 17:16:57 -0400

    finished for now

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Reddit Score Tracker
 
+This repo contains a Python script to collect reddit post score data, writing it to a CSV file. The CSV file will contain the timestamp of when the data was collected as well as the score that the post had at the time.
+
+This data can then be used with a simple R program that's included. It uses ggplot to produce a scatter plot of the posts score over time.
--- plot.r
+++ plot.r
@@ -1,6 +1,6 @@
 library(tidyverse)
-library(anytime)
 
+# Read the csv from the python script
 data <- read_csv("score_history.csv")
 
 first <- data$Time[1] # The time of the first piece of data
@@ -10,13 +10,15 @@ minute <- 60
 hour <- minute * 60
 day <- hour * 24
 
-# Change to [minute,hour,day] depending on how you want the graph 
-# to be produced. Don't forget to change the x-label below
+# Adds a new column quantifying the time elapsed since the 
+# first data point.Change to [minute,hour,day] depending on 
+# how you want the graph to be produced. Don't forget to 
+# change the x-label below
 data <- data %>%
   mutate(TimeSinceFirst = (Time - first) / hour)
 
 
-# Change x-label to match units of TimeSinceFirst
+# Creates the plot. Change x-label to match units of TimeSinceFirst
 data %>%
   ggplot(aes(x=TimeSinceFirst, y=Score)) +
   geom_point() + 
