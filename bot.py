b48a56a88064c9fcd874ec192e01fef15dc97918
dylan weaver <dweaver@umd.edu>
2019-06-21 13:58:22 -0400

    add better exception handling

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import praw, csv, time, sys
+from prawcore import PrawcoreException
 
 def authenticate():
     """Logs into Reddit through PRAW so data collection can start."""
@@ -6,10 +7,10 @@ def authenticate():
         reddit = praw.Reddit('upvotetrack', user_agent="Score tracker")
         return reddit
     except praw.exceptions.ClientException:
-        print("Invalid praw.ini config.")
-        sys.exit(1)
+        raise
 
 def run(reddit, submission_url):
+    print("Starting!")
     try:
         with open('score_history.csv', 'w', newline='') as score_file:
             writer = csv.writer(score_file)
@@ -18,12 +19,22 @@ def run(reddit, submission_url):
             while True:
                 submission = reddit.submission(url=submission_url)
                 writer.writerow([time.time(), submission.score])
+                print(f"Wrote {time.time()}, {submission.score}")
                 time.sleep(10)
+    except PrawcoreException as e:
+        raise e
     except TypeError:
-        print("Invalid URL provided.")
-        sys.exit(1)
+        raise
 
 if __name__ == "__main__":
-    reddit = authenticate()
-    url = "https://www.reddit.com/r/formula1/comments/bybint/sebastian_vettel_takes_pole_position_at_the_2019/"
-    run(reddit, url)
+    try:
+        reddit = authenticate()
+        url = ""
+        run(reddit, url)
+    except praw.exceptions.ClientException:
+        print("There's an error in your `praw.ini` file.")
+    except PrawcoreException as e:
+        print("Error:", e, ". Check your link or `praw.ini` file.")
+    except TypeError:
+        print("Invalid URL provided.")
+    # set up args for url
